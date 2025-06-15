3e29d2df0069d214bf3e37f647ce07660ca7bb71
blockche <blockche@qq.com>
2025-06-15 17:04:20 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
-  { "folke/tokyonight.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    -- opts = {
+    --   -- style = "night", -- 可选：night/moon/storm/day
+    --   transparent = true, -- 启用透明度
+    --   styles = {
+    --     sidebars = "transparent", -- 侧边栏透明
+    --     floats = "transparent", -- 浮动窗口透明
+    --   },
+    -- },
+  },
 
   {
     "LazyVim/LazyVim",
