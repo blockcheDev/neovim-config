a125d6493b8fd6377b18abec6cd95742763d8773
blockche <blockche@qq.com>
2025-06-18 20:11:46 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,5 @@ vim.opt.tabstop = 4 -- Tab 字符显示为 4 空格
 vim.opt.softtabstop = 4 -- 按 Tab/Backspace 时操作 4 空格
 vim.opt.expandtab = true -- Tab 键输入空格而非制表符
 vim.opt.smartindent = true -- 智能缩进（如 C/C++ 等语言）
+
+vim.g.autoformat = false
